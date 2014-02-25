3f2fe23ed96d5eab63d9ce91516964674c507228
contessa <tessa.fallon@gmail.com>
2014-02-25 15:30:37 -0500

    completed d3 UK map

---
+++ chart.js
@@ -0,0 +1,12 @@
+var data = [4, 8, 15, 3, 23, 42, 56];
+
+var x = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range([0, 420]);
+
+d3.select(".chart")
+  .selectAll("div")
+    .data(data)
+  .enter().append("div")
+    .style("width", function(d) { return x(d) + "px"; })
+    .text(function(d) { return d; });
